150022ca690b334dd02127ab6f3eca0637b05da1
saurabh <behl.saurabh@gmail.com>
2019-03-02 11:53:24 +0530

    updated Code

--- code.php
+++ code.php
@@ -6,14 +6,15 @@ $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("databaseusername");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-if ($connection->connect_errno) {
+
+$connection =  mysqli_connect("localhost", "first", "first", "first");
+if (!$connection) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 } else {
     printf("Connected to the database");
 }
-$connection->close();
+mysqli_close();
 
 ?>
        
