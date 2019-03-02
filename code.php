6295c61933aa1edb7c17a3a2e881bbedb95bb3c2
saurabh <behl.saurabh@gmail.com>
2019-03-02 13:42:11 +0530

    updated Code

--- code.php
+++ code.php
@@ -3,9 +3,9 @@
 
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("first");
-$dbpwd = getenv("first");
-$dbname = getenv("first");
+$dbuser = getenv("databaseuser");
+$dbpwd = getenv("databasepassword");
+$dbname = getenv("databasename");
 
 
 echo "user ".$dbuser." ".$dbpwd." dbname ".$dbname." port  ".$dbport;
